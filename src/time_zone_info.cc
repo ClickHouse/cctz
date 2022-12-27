7b5e56164b812f5cc105c70b13ea5fc012908b44
avogar <pav.cruglov@yandex.ru>
2022-12-27 17:41:51 +0000

    Prevent arbitrary file open while loading timezone

--- src/time_zone_info.cc
+++ src/time_zone_info.cc
@@ -806,6 +806,21 @@ std::unique_ptr<ZoneInfoSource> FuchsiaZoneInfoSource::Open(
   return nullptr;
 }
 
+bool CheckTimeZoneName(const std::string& name) {
+    const char * forbidden_beginnings[] = {"/", "./", "~", ".."};
+    for (const auto & pattern : forbidden_beginnings) {
+        if (name.starts_with(pattern)) {
+            return false;
+        }
+    }
+
+    if (name.find("../") != std::string::npos) {
+        return false;
+    }
+
+    return true;
+}
+
 }  // namespace
 
 bool TimeZoneInfo::Load(const std::string& name) {
@@ -816,6 +831,12 @@ bool TimeZoneInfo::Load(const std::string& name) {
   auto offset = seconds::zero();
   if (FixedOffsetFromName(name, &offset)) {
     return ResetToBuiltinUTC(offset);
+  }
+
+  // Check if timezone name contains forbidden patterns
+  // that can lead to arbitrary file open.
+  if (!CheckTimeZoneName(name)) {
+      return false;
   }
 
   // Find and use a ZoneInfoSource to load the named zone.
