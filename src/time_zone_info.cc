6440590af6962a2ed22ef574c2289a54ccf165fb
avogar <pav.cruglov@yandex.ru>
2022-12-27 17:41:51 +0000

    Prevent arbitrary file open while loading timezone

--- src/time_zone_info.cc
+++ src/time_zone_info.cc
@@ -818,6 +818,21 @@ bool TimeZoneInfo::Load(ZoneInfoSource* zip) {
   return true;
 }
 
+bool CheckTimeZoneName(const std::string& name) {
+    const char * forbidden_beginnings[] = {"/", "./", "~", ".."};
+    for (const auto & pattern : forbidden_beginnings) {
+        if (name.starts_with(pattern)) {
+            return false;
+        }
+    }
+
+    if (name.find("../") != std::string::npos) {
+        return false;
+    }
+
+    return true;
+}
+
 bool TimeZoneInfo::Load(const std::string& name) {
   // We can ensure that the loading of UTC or any other fixed-offset
   // zone never fails because the simple, fixed-offset state can be
@@ -826,6 +841,12 @@ bool TimeZoneInfo::Load(const std::string& name) {
   auto offset = seconds::zero();
   if (FixedOffsetFromName(name, &offset)) {
     return ResetToBuiltinUTC(offset);
+  }
+
+  // Check if timezone name contains forbidden patterns
+  // that can lead to arbitrary file open.
+  if (!CheckTimeZoneName(name)) {
+      return false;
   }
 
   // Find and use a ZoneInfoSource to load the named zone.
